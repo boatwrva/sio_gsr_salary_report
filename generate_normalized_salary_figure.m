9fb9363196630ebdf70933c9ba946b89a0d59d5a
haydenallenjohnson <h.johnson1701@gmail.com>
2024-02-25 17:26:14 -0800

    Add files via upload

--- generate_normalized_salary_figure.m
+++ generate_normalized_salary_figure.m
@@ -1,5 +1,5 @@
 % Scrpit to create two-panel figure showing inflation-adjusted and
-% rent-adjusted normalized GSR salary at SIO from September 2016 to May
+% rent-adjusted normalized GSR salary at SIO from January 2015 to May
 % 2025. Takes "salaries.txt" as input, which is a timeseires of salary data
 % generated using the script "generate_salary_timeseries.m". Also takes CPI
 % data downloaded from the Bureau of Labor Statistics, and housing rental
@@ -24,7 +24,7 @@ bar_offset = caldays(-15); % to make the plots look nice
 % subjects > consumer price index > cpi data > regional resources > west
 % https://data.bls.gov/pdq/SurveyOutputServlet?data_tool=dropmap&series_id=CUUR0400SA0,CUUS0400SA0
 cpi = zeros(size(t));
-cpi_table = readtable('cpi_western_urban_dec2023.xlsx');
+cpi_table = readtable('cpi_western_urban_jan2024.xlsx');
 cpi_array = table2array(cpi_table(:,2:13));
 for i = 1:length(t)
     for j = 1:length(cpi_table.Year)
@@ -69,10 +69,10 @@ vertical_bar_col = 0.7*[1 1 1];
 % plot cpi-normalized gsr salary
 figure(1);
 subplot(2,1,1);
-ylimits = [0.85 1.28];
+ylimits = [0.82 1.17];
 plot([contract_start_time contract_start_time]+bar_offset,ylimits,'color',vertical_bar_col,'linewidth',2);
 hold on;
-plot([settlement_time settlement_time]+bar_offset,ylimits,'color',vertical_bar_col,'linewidth',2);
+% plot([settlement_time settlement_time]+bar_offset,ylimits,'color',vertical_bar_col,'linewidth',2);
 
 h4 = plot(t,cpi_normalized_salary_new_student_postqual_historical,'color',new_student_postqual_col,'linewidth',2);
 h3 = plot(t,cpi_normalized_salary_new_student_prequal_historical,'color',new_student_prequal_col,'linewidth',2);
@@ -87,10 +87,9 @@ plot(t,cpi_normalized_salary_old_student_prequal_projected,'color',old_student_p
 h5 = plot(NaN,NaN,'color','black','linewidth',2);
 h6 = plot(NaN,NaN,'color','black','linewidth',2,'linestyle','--');
 hold off;
-text(datetime(2023,5,15),0.98,'Initial contract implementation','rotation',90,'fontsize',11);
-text(datetime(2024,5,15),1.03,'Settlement','rotation',90,'fontsize',11);
+text(datetime(2023,1,15),0.89,'Contract wages article takes effect','rotation',90,'fontsize',11);
+text(datetime(2024,1,15),1.0,'Settlement','rotation',90,'fontsize',11);
 ylim(ylimits);
-yticks(0.9:0.05:1.25);
 ylabel('Inflation-adjusted normalized salary, S_I');
 legend([h1 h2 h3 h4 h5 h6],{'Old Students, pre-qualification','Old Students, post-qualification','New Students, pre-qualification','New Students, post-qualification','Historical','Projected'},'location','northwest');
 grid on;
@@ -101,9 +100,9 @@ set(gca,'position',[0.1 0.55 0.84 0.4]);
 % ZORI (smoothed): All homes plus multifamily time series, by city
 % downloaded from https://www.zillow.com/research/data/
 % need to look manually in the file to see start and stop times
-zori_table = readtable('zori_unadjusted_all_homes_plus_multifamily_by_city_dec2023.csv');
+zori_table = readtable('zori_unadjusted_all_homes_plus_multifamily_by_city_jan2024.csv');
 zori_san_diego_raw = table2array(zori_table(10,9:end))';
-zori_time = (datetime(2015,01,01):calmonths(1):datetime(2023,12,1))'; 
+zori_time = (datetime(2015,01,01):calmonths(1):datetime(2024,1,1))'; 
 
 % put zori onto same time stamp
 zori = NaN(size(t));
@@ -132,10 +131,10 @@ rent_normalized_salary_new_student_postqual_projected = (gsr_salary_new_student_
 
 % plot rent-normalized gsr salary
 subplot(2,1,2);
-ylimits = [0.7 1.08];
+ylimits = [0.63 1.02];
 plot([contract_start_time contract_start_time]+bar_offset,ylimits,'color',vertical_bar_col,'linewidth',2);
 hold on;
-plot([settlement_time settlement_time]+bar_offset,ylimits,'color',vertical_bar_col,'linewidth',2);
+% plot([settlement_time settlement_time]+bar_offset,ylimits,'color',vertical_bar_col,'linewidth',2);
 
 h3 = plot(t,rent_normalized_salary_new_student_prequal_historical,'color',new_student_prequal_col,'linewidth',2);
 h4 = plot(t,rent_normalized_salary_new_student_postqual_historical,'color',new_student_postqual_col,'linewidth',2);
@@ -154,7 +153,8 @@ ylabel('Rent-adjusted normalized salary, S_R');
 set(gca,'fontsize',12);
 set(gca,'position',[0.1 0.06 0.84 0.4]);
 
+f = gcf;
 annotation('textbox',[0.05 0.89 0.1 0.1],'string','(a)','fontsize',14,'edgecolor','none','fontweight','bold');
 annotation('textbox',[0.05 0.4 0.1 0.1],'string','(b)','fontsize',14,'edgecolor','none','fontweight','bold');
-set(gcf,'position',[360 42 760 740]);
-saveas(1,'combined_normalized_gsr_salary.png');
+set(f,'position',[360 42 760 740]);
+exportgraphics(f,'combined_normalized_gsr_salary.pdf');
--- generate_salary_timeseries.m
+++ generate_salary_timeseries.m
@@ -4,7 +4,7 @@
 % "generate_normalized_salary_figure.m"
 
 % create datetime array
-t_start = datetime(2016,09,01);
+t_start = datetime(2015,01,01);
 t_end = datetime(2025,05,01);
 t = (t_start:calmonths(1):t_end)';
 
@@ -18,7 +18,8 @@ t_apr23_raise = datetime(2023,04,01);
 t_oct23_raise = datetime(2023,10,01);
 t_oct24_raise = datetime(2024,10,01);
 t_new_students_start = datetime(2023,09,01);
-t_settlement = datetime(2024,04,01);
+t_settlement_start = datetime(2024,04,01);
+t_settlement_end = datetime(2024,07,01);
 
 % initialize arrays
 gsr_salary_old_student_prequal = zeros(size(t));
@@ -49,16 +50,18 @@ gsr_salary_old_student_prequal(t<t_2019_raise) = 30000;
 gsr_salary_old_student_prequal(t_2019_raise<=t & t<t_2021_raise) = 32000;
 gsr_salary_old_student_prequal(t_2021_raise<=t & t<t_2022_raise) = 33000;
 gsr_salary_old_student_prequal(t_2022_raise<=t & t<t_apr23_raise) = 34000;
-gsr_salary_old_student_prequal(t_apr23_raise<=t & t<t_settlement) = 0.4*step6_fte(t_apr23_raise<=t & t<t_settlement);
-gsr_salary_old_student_prequal(t_settlement<=t) = 0.5*step6_fte(t_settlement<=t);
+gsr_salary_old_student_prequal(t_apr23_raise<=t & t<t_settlement_start) = 0.4*step6_fte(t_apr23_raise<=t & t<t_settlement_start);
+gsr_salary_old_student_prequal(t_settlement_start<=t & t<t_settlement_end) = 0.5*step6_fte(t_settlement_start<=t & t<t_settlement_end);
+gsr_salary_old_student_prequal(t_settlement_end<=t) = 0.4*step6_fte(t_settlement_end<=t);
 
 % specify old student post-qualification salary
 gsr_salary_old_student_postqual(t<t_2019_raise) = 31000;
 gsr_salary_old_student_postqual(t_2019_raise<=t & t<t_2021_raise) = 33000;
 gsr_salary_old_student_postqual(t_2021_raise<=t & t<t_2022_raise) = 34000;
 gsr_salary_old_student_postqual(t_2022_raise<=t & t<t_apr23_raise) = 35000;
-gsr_salary_old_student_postqual(t_apr23_raise<=t & t<t_settlement) = 0.43*step6_fte(t_apr23_raise<=t&t<t_settlement);
-gsr_salary_old_student_postqual(t_settlement<=t) = 0.5*step6_fte(t_settlement<=t);
+gsr_salary_old_student_postqual(t_apr23_raise<=t & t<t_settlement_start) = 0.43*step6_fte(t_apr23_raise<=t&t<t_settlement_start);
+gsr_salary_old_student_postqual(t_settlement_start<=t & t<t_settlement_end) = 0.5*step6_fte(t_settlement_start<=t & t<t_settlement_end);
+gsr_salary_old_student_postqual(t_settlement_end<=t) = 0.43*step6_fte(t_settlement_end<=t);
 
 % specify new student pre-qualification salary
 gsr_salary_new_student_prequal(t<t_new_students_start) = NaN;
